f8eb34740390cdc14c1f902631358042d1990552
sunrabbit123 <qudwls185@naver.com>
2021-06-02 13:55:35 +0900

    fix: set status function return value is <DocumentType<Post>>

--- src/model/schema/posts.ts
+++ src/model/schema/posts.ts
@@ -2,9 +2,12 @@ import {
   getModelForClass,
   prop,
   DocumentType,
-  modelOptions,
+  modelOptions
 } from "@typegoose/typegoose";
 import { Schema } from "mongoose";
+import * as crypto from "crypto";
+import { Base64 } from "js-base64";
+import { ModelType } from "@typegoose/typegoose/lib/types";
 
 export enum PostStatus {
   Pending = "PENDING",
@@ -12,7 +15,6 @@ export enum PostStatus {
   Rejected = "REJECTED",
   Deleted = "DELETED",
 }
-
 export interface PostRequestForm {
   title: string;
   content: string;
@@ -21,12 +23,15 @@ export interface PostRequestForm {
 export interface PublicPostFields {
   id: string;
   number?: number;
-  title?: string;
+  title: string;
   content: string;
   tag: string;
-  FBLink: string;
+  FBLink?: string;
   createdAt: number;
   status: string;
+}
+export interface PostAuthorFields extends PublicPostFields {
+  hash: string;
 }
 
 @modelOptions({
@@ -45,9 +50,11 @@ export interface PublicPostFields {
     },
   },
 })
-export class _Post {
+export class Post {
   public _id: Schema.Types.ObjectId;
-  public createdAt: Date = new Date();
+
+  @prop({ default: new Date() })
+  public createdAt: Date;
 
   @prop()
   public number?: number;
@@ -70,52 +77,96 @@ export class _Post {
   @prop()
   public FBLink?: string;
 
+  @prop({
+    default: (): string => {
+      return crypto
+        .createHash("sha256")
+        .update(Date.now().toString())
+        .digest("hex");
+    },
+  })
+  public hash: string;
+
+  public get cursorId(): string {
+    return Base64.encode(this._id.toString());
+  }
   public get id(): Schema.Types.ObjectId {
     return this._id;
   }
 
-  public async setAccepted(this: DocumentType<_Post>): Promise<boolean> {
-    try {
-      this.status = PostStatus.Accepted;
-      const lastPost = (
-        await Post.find().sort({ number: -1 }).limit(1).exec()
-      )[0];
-      this.number = (lastPost.number ?? 0) + 1;
-      await this.save();
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-    return true;
+  public async setAccepted(this: DocumentType<Post>): Promise<DocumentType<Post>> {
+  
+    this.status = PostStatus.Accepted;
+    const lastPost = (
+      await PostModel.find().sort({ number: -1 }).limit(1).exec()
+    )[0];
+    this.number = (lastPost.number ?? 0) + 1;
+    (await this.save());
+    return this;
+  
   }
 
   public async setRejected(
-    this: DocumentType<_Post>,
+    this: DocumentType<Post>,
     reason: string
-  ): Promise<boolean> {
-    try {
-      this.status = PostStatus.Rejected;
-      this.reason = reason;
-      await this.save();
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-    return true;
+  ): Promise<DocumentType<Post>> {
+    this.status = PostStatus.Rejected;
+    this.reason = reason;
+    await this.save();
+    return this;
   }
 
-  public async setDeleted(this: DocumentType<_Post>): Promise<boolean> {
-    try {
-      this.status = PostStatus.Deleted;
-      await this.save();
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-    return true;
+  public async setDeleted(this: DocumentType<Post>): Promise<DocumentType<Post>> {
+    this.status = PostStatus.Deleted;
+    await this.save();
+    return this;
+  }
+
+  public getAuthorFields(this: DocumentType<Post>): PostAuthorFields {
+    return {
+      id: this.id,
+      number: this.number,
+      title: this.title,
+      content: this.content,
+      tag: this.tag,
+      FBLink: this.FBLink,
+      createdAt: this.createdAt.getTime(),
+      status: this.status,
+      hash: this.hash,
+    };
+  }
+  public getPublicFields(this: DocumentType<Post>): PublicPostFields {
+    return {
+      id: this.id,
+      number: this.number,
+      title: this.title,
+      content: this.content,
+      tag: this.tag,
+      FBLink: this.FBLink,
+      createdAt: this.createdAt.getTime(),
+      status: this.status,
+    };
+  }
+  public static async getList(
+    this: ModelType<Post> & typeof Post,
+    count: number = 10,
+    cursor: number = 0
+  ): Promise<Array<DocumentType<Post>>> {
+    const condition = cursor === 0 ? findCondition : {
+      number: { $lt: cursor ?? 0 },
+      status: PostStatus.Pending,
+    };
+    // TODO 리팩토링 추후 필요
+    
+    const posts = await this.find(condition)
+      .sort({ number: -1 })
+      .limit(count)
+      .exec();
+    return posts;
   }
 }
-
-const Post = getModelForClass(_Post);
-
-export default Post;
+const findCondition ={
+  status: PostStatus.Pending
+};
+const PostModel = getModelForClass(Post);
+export default PostModel
